711c19c509176951117d7a9909ed8c98f821a520
Rishabh gajra <rgrishigajra@gmail.com>
2020-05-23 16:54:24 -0400

    adding lists function, making relationships

--- todo.py
+++ todo.py
@@ -14,17 +14,33 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.String(), nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey(
+        "todolists.id"), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
 
+class TodoList(db.Model):
+    __tablename__ = "todolists"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref="todolist", lazy=True)
 # db.create_all()  # creating all tables
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by("id").all())
+    return redirect(url_for('lists', list_id=1))
+
+
+@app.route('/lists/<list_id>')
+def lists(list_id):
+     
+     return render_template('index.html',todos=Todo.query.filter_by(list_id=list_id).order_by("id").all(),
+     active_list=TodoList.query.get(list_id),
+     todolists=TodoList.query.all())
+
 
 
 @app.route('/todo/create', methods=['POST'])
@@ -51,7 +67,7 @@ def create_todo():
 
 @app.route("/todo/<todo_id>/check", methods=['POST'])
 def check_box(todo_id):
-    print(request.get_json()['checked'],todo_id)
+    print(request.get_json()['checked'], todo_id)
     body = {}
     try:
         checked = request.get_json()['checked']
@@ -67,3 +83,18 @@ def check_box(todo_id):
     finally:
         db.session.close()
     return jsonify(body)
+
+
+@app.route("/todo/<todo_id>/delete", methods=["GET"])
+def delete(todo_id):
+    print(todo_id)
+    try:
+        todo = Todo.query.get(todo_id)
+        db.session.delete(todo)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        print(sys.exc_info)
+    finally:
+        db.session.close()
+    return "ok"
